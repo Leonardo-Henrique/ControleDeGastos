0e045e67931ec4e523b399219c6b91a934d92957
Leonardo Henrique <leonardoh481@gmail.com>
2021-01-24 16:51:10 -0300

    CSS added to dashboard

--- dashboard.php
+++ dashboard.php
@@ -18,12 +18,143 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Dashboard</title>
 
+        <link rel="stylesheet" type="text/css" href="css/dashboard-style.css" />
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;400;500;600&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/9ac6cfa819.js" crossorigin="anonymous"></script>
+
+
         
     </head>
 
     <body>
+        
+        <div class="container">
 
-        <a href="sair.php">Sair</a>
+            <div class="col-left">
+
+                <nav class="menu">
+
+                    <li><i class="fas fa-home" style="color: #b7b7b7;"></i><a href="">Dashboard</a></li>
+                    <li><i class="fas fa-copy" style="color: #b7b7b7;"></i><a href="">Contas</a></li>
+                    <li><i class="fas fa-cog" style="color: #b7b7b7;"></i><a href="">Configurações</a></li>
+                    <li><i class="fas fa-power-off" style="color: #b7b7b7;"></i><a href="sair.php">Sair</a></li>
+
+                </nav>
+            
+            </div><!-- LEFT COLUMN -->
+
+            <div class="col-right">
+
+                <div class="header">
+
+                    <div class="header-l">
+                        <form class="search">
+                        <i class="fas fa-search"></i><input type="text" placeholder="Pesquisar por contas">
+                            <!-- <input type="submit">    -->
+                        </form>
+                        <!-- <h1>Olá, Leonardo!</h1>
+                        <p>O que vamos fazer hoje?</p> -->
+                    </div>
+
+                    <div class="header-r">
+                        <div class="user">
+                            <span>Leonardo Henrique</span>
+                        </div>
+                    </div>
+                
+                </div>
+
+                <div class="clear"></div>
+
+                <div class="resume">
+
+                    
+
+
+                    <div class="resume-l">
+
+                        <span>Resumo</span><br><br>
+
+                        <div class="r-l-b1">
+
+                            <span class="legend">contas em aberto</span><br>
+                            <span class="value">R$ 330</span>
+
+                        </div>
+
+                        <div class="r-l-b1">
+
+                            <span class="legend">em conta</span><br>
+                            <span class="value">R$ 1330</span>
+                            
+                        </div>
+
+                    </div>
+
+                    <div class="clear"></div>
+                </div>
+
+                <div class="bills">
+                    
+                    <div class="bills-header">
+
+                        <span class="left-span">Suas contas</span>
+                        <span class="right-span"><a href="">Ver tudo</a></span>
+
+                    </div>
+
+                    <table>
+                        <tr>
+                            <th>Data</th>
+                            <th>Descrição</th>
+                            <th>Valor</th>
+                        </tr>
+
+                        <tr>
+                            <td>23 de junho</td>
+                            <td>Conta de luz</td>
+                            <td>R$ 225,20</td>
+                        </tr>
+
+                        <tr>
+                            <td>23 de junho</td>
+                            <td>Conta de luz</td>
+                            <td>R$ 225,20</td>
+                        </tr>
+
+                        <tr>
+                            <td>23 de junho</td>
+                            <td>Conta de luz</td>
+                            <td>R$ 225,20</td>
+                        </tr>
+
+                        <tr>
+                            <td>23 de junho</td>
+                            <td>Conta de luz</td>
+                            <td>R$ 225,20</td>
+                        </tr>
+
+                        <tr>
+                            <td>23 de junho</td>
+                            <td>Conta de luz</td>
+                            <td>R$ 225,20</td>
+                        </tr>
+                    </table>
+
+                </div>
+
+            </div><!-- RIGHT COLUMN -->
+        
+        
+        </div>
+
+
+
+
+
+
+        <!-- <a href="sair.php">Sair</a>
 
         <h1>Cadastrar conta</h1>
 
@@ -37,7 +168,7 @@
 
         <div class="retorno">
             
-        </div>
+        </div> -->
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="js/script.js"></script>
